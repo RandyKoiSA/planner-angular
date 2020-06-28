dae4d9f4de1c11c234b94fcd36f321e45e177252
Randy Le <97randy.le@gmail.com>
2020-06-27 17:55:54 -0700

    added bootstrap to header component and planner component

--- src/app/components/planner/planner.component.ts
+++ src/app/components/planner/planner.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlannerComponent implements OnInit {
 
+  planners = [
+    {
+      "id": 1,
+      "title": "Planner 1",
+      "startdate": "2000-03-24",
+      "enddate": "2011-04-25",
+      "description": "This is the description of the planner"
+    },
+    {
+      "id": 2,
+      "title": "Planner 2",
+      "startdate": "2000-03-24",
+      "enddate": "2011-04-25",
+      "description": "This is the description of the planner"
+    },
+    {
+      "id": 3,
+      "title": "Planner 3",
+      "startdate": "2000-03-24",
+      "enddate": "2011-04-25",
+      "description": "This is the description of the planner"
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
